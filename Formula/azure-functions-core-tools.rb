4015e8cb40e7c9278441ff2ecea502cf5aab9809
Lukasz Stempniewicz <lukaszs@Lukaszs-MacBook-Air.local>
2018-02-26 17:04:42 -0800

    1 issue with audit=>beta url

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -1,26 +1,22 @@
- class AzureFunctionsCoreTools < Formula
+class AzureFunctionsCoreTools < Formula
   desc "Azure Function Cli 2.0"
   homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
-  url "https://ci.appveyor.com/api/buildjobs/4i2wiosxpehv6wqm/artifacts/deploy%2FAzure.Functions.Cli.osx-x64.zip"
+  url "https://functionscdn.azureedge.net/public/2.0.1-beta.22/osx/azure.functions.cli.osx-x64.2.0.1-beta.22.zip"
+  version "2.0.1-beta.22"
   sha256 "d99f03b54c399479246d6c395dd1fa93092769796e7a5bbd5c6b5da1c07fd766"
   head "https://github.com/Azure/azure-functions-core-tools"
-  version "2.0.1-beta.22"
- 
-   def install
+
+  bottle :unneeded
+
+  def install
     prefix.install Dir["*"]
-    chmod 0555, prefix/'func'
-    bin.install_symlink prefix/'func'
+    chmod 0555, prefix/"func"
+    bin.install_symlink prefix/"func"
   end
-   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test azure-functions-core-tools`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/func")
+    system bin/"func", "new", "-l", "C#", "-t", "HttpTrigger", "-n", "confusedDevTest"
+    assert_predicate testpath/"confusedDevTest/function.json", :exist?
   end
 end
