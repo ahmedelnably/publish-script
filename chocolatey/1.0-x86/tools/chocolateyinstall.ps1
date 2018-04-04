e28598120d5a6b480ca7508ce5fd56ea3b4f0f4b
ShunXian Cai <shucai@microsoft.com>
2018-04-04 16:21:25 -0700

    add chocolatey scripts

---
+++ chocolatey/1.0-x86/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'azure-functions-core-tools'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://functionscdn.azureedge.net/public/1.0.9/Azure.Functions.Cli.zip'
+$checksum 	= '8803F46F6B3F467BDF401FBA0EFB6E08644A1DF77434BEC65159512BB68A3579B37BE9A09E8B54591BC6973FC63AE17D64D7A7484B383EAE8F670CC25053DC8E'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'SHA512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# only symlink for func.exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  if (!$file.Name.Equals("func.exe")) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
+}
---
+++ chocolatey/2.0-x86/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'azure-functions-core-tools'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://functionscdn.azureedge.net/public/2.0.1-beta.25/Azure.Functions.Cli.win-x86.2.0.1-beta.25.zip'
+$checksum 	= '49A54FA96D8FDC32BDDE691B4977CEEAF708D4B38AE4C4E75B1913BC1D59CC8E5714FF09FB5EB990928558FF8C48FCFD89DCE309E3544168A077FCA8E3A96613'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'SHA512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# only symlink for func.exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  if (!$file.Name.Equals("func.exe")) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
+}
