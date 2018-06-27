fc565bbf0278111b2dd17cd149c88b128bd3b8f1
shun <mike53669@gmail.com>
2018-06-26 18:22:14 -0700

    use ~beta for debian package version

--- ubuntu/publishScript.py
+++ ubuntu/publishScript.py
@@ -11,10 +11,15 @@ import shutil
 import subprocess
 import datetime
 
-# example: 2.0.1-beta.25
+# input example: 2.0.1-beta.25
 version = sys.argv[1]
 fileName = f"Azure.Functions.Cli.linux-x64.{version}.zip"
 url = f'https://functionscdn.azureedge.net/public/{version}/{fileName}'
+
+# version used in url is provided from user input
+# version used for packaging .deb package needs a slight modification
+# for beta, change to tilde, so it will be placed before rtm versions in apt
+debianVersion = f'{version.replace("-beta", "~beta")}-1'
 
 # download the zip
 # output to local folder, using name fileName
@@ -32,7 +37,7 @@ if os.path.exists(outputDir):
 # output dir is empty
 # all directories path are relative
 packageName = "azure-functions-core-tools"
-root = f"{outputDir}/{packageName}_{version}-1"
+root = f"{outputDir}/{packageName}_{debianVersion}"
 usr = f"{root}/usr"
 usrlib = f"{usr}/lib"
 usrlibFunc = f"{usrlib}/{packageName}"
@@ -53,7 +58,7 @@ os.symlink(f"../lib/{packageName}/func","func")
 # go back to root
 os.chdir("../..")
 # strip sharedobjects
-subprocess.run("find * -type f | xargs strip --strip-unneeded", shell=True)
+subprocess.run("find -name *.so | xargs strip --strip-unneeded", shell=True)
 
 document = f"usr/share/doc/{packageName}"
 os.makedirs(document)
@@ -65,7 +70,7 @@ with open("../../changelog_template") as f:
     stringData = f.read()
 # changelog, dateTime: Tue, 06 April 2018 16:32:31
 t = datetime.datetime.utcnow().strftime("%a, %d %b %Y %X")
-formattedString = stringData.format(VERSION=version,DATETIME=t)
+formattedString = stringData.format(DEBIANVERSION=debianVersion,DATETIME=t,VERSION=version, PACKAGENAME=packageName)
 with open(f"{document}/changelog.Debian","w") as f:
     print(f"writing changelog with date utc: {t}")
     f.write(formattedString)
@@ -81,7 +86,7 @@ subprocess.run("find usr/* -type f | xargs md5sum > DEBIAN/md5sums", shell=True)
 # produce the control file from template
 with open("../../control_template") as f:
     stringData = f.read()
-formattedString = stringData.format(VERSION=version)
+formattedString = stringData.format(DEBIANVERSION=debianVersion, PACKAGENAME=packageName)
 with open("DEBIAN/control","w") as f:
     print("trying to write control file...")
     f.write(formattedString)
@@ -99,4 +104,6 @@ os.chmod("usr/bin/func", 0o755)
 # go to output
 os.chdir("../")
 print("trying to produce deb file...")
-subprocess.run(f"fakeroot dpkg-deb --build {packageName}_{version}-1", shell=True)
+subprocess.run(f"fakeroot dpkg-deb --build {packageName}_{debianVersion}", shell=True)
+
+# TODO test func executables
